f4cde00dffefa06667b277473179610ef44748de
Joel Santos <joeloliveira56@gmail.com>
2026-03-09 21:34:06 -0300

    feat: Atualização API Fullstack - Listar Alunos

--- aula01/back/app.js
+++ aula01/back/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import db from "./src/config/db.js";
 import alunoRoutes from "./src/routes/alunoRoutes.js";
+
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(
+    cors(
+        {
+            origin: process.env.API_URL_FRONT
+        }
+    )
+);
 app.use("/alunos", alunoRoutes);
 
 app.get("/", (req, res)=>{
