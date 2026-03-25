189eed2121db91787cbc096df12a6456f270438d
Joel Santos <joeloliveira56@gmail.com>
2026-03-24 21:31:06 -0300

    feat: Adiciona lógica de autenticação de usuário e rotas correspondentes

--- aula02/back/src/controllers/UsuarioController.js
+++ aula02/back/src/controllers/UsuarioController.js
@@ -41,18 +41,89 @@ export class UsuarioController {
             res.status(500).json({ msg: "Erro interno ao cadastrar usuário", erro: error.message });
         }
     }
-    static async buscarPorId(req, res){
+    static async buscarPorId(req, res) {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
             const result = await UsuarioModel.buscarPorId(id);
             const usuario = result.rows[0];
-            if(!usuario){
-                res.status(404).json({msg: "Usuário não encontrado!"});
+            if (!usuario) {
+                res.status(404).json({ msg: "Usuário não encontrado!" });
                 return;
             }
-            res.status(200).json({msg: "Usuário encontrado", usuario});
+            res.status(200).json({ msg: "Usuário encontrado", usuario });
         } catch (error) {
-            res.status(500).json({msg: "Erro interno ao buscar usuário", erro: error.message});
+            res.status(500).json({ msg: "Erro interno ao buscar usuário", erro: error.message });
         }
     }
+
+    static async atualizarUsuario(req, res) {
+        try {
+            const { id } = req.params;
+            const { nome, email, senha } = req.body;
+            if (!nome || !email || !senha) {
+                res.status(400).json({ msg: "Preencha todos os campos" });
+                return;
+            }
+            const senhaHash = await bcrypt.hash(senha, parseInt(process.env.SALT));
+            const result = await UsuarioModel.atualizarUsuario(id, nome, email, senhaHash, "USER");
+            if (result.rowCount === 0) {
+                res.status(404).json({ msg: "Usuário não encontrado" });
+                return;
+            }
+            res.status(200).json({ msg: "Usuário atuaizado com sucesso" });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno ao ataulizar usuário", erro: error.message });
+        }
+    }
+    static async deletarUsuario(req, res) {
+        try {
+            const { id } = req.params;
+            const result = await UsuarioModel.deletarUsuario(id);
+            if (result.rowCount === 0) {
+                res.status(404).json({ msg: "Usuário não encontrado." });
+                return;
+            }
+            res.status(200).json({ msg: "Usuário deletado com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno ao deletar usuário", erro: error.message });
+        }
+    }
+    static async loginUsuario(req, res) {
+        try {
+            const { email, senha } = req.body;
+            if (!email || !senha) {
+                res.status(400).json({ msg: "Todos os campos devem ser preenchidos" });
+                return;
+            }
+            const result = await UsuarioModel.buscarPorEmail(email);
+            const usuario = result.rows[0];
+            if (!usuario) {
+                res.status(401).json({ msg: "Email ou senha inválidos!" });
+                return
+            }
+            const senhaValida = await bcrypt.compare(senha, usuario.senha);
+            if (!senhaValida) {
+                res.status(401).json({ msg: "Email ou senha inválidos!" });
+                return
+            }
+            const token = jwt.sign(
+                {
+                    id: usuario.id,
+                    nome: usuario.nome,
+                    role: usuario.role
+                },
+                process.env.JWT_SECRET,
+                {expiresIn: "1h"}
+            )
+            res.cookie("token", token,{
+                httpOnly: true,
+                sameSite: "lax",
+                maxAge: 60*60*1000 //1h
+            });
+            res.status(200).json({msg:"Login realizado com sucesso", usuario: usuario.nome, token: token});
+        } catch (error) {
+            res.status(500).json({msg: "Erro interno ao realizar login", erro: error.message});
+        }
+    }
+
 }
