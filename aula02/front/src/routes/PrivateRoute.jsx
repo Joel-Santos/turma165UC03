69acc5f41672202c835cf649090ba6e1188cf080
Joel Santos <joeloliveira56@gmail.com>
2026-03-31 15:07:35 -0300

    feat: adiciona verificação de função de usuário e redirecionamento apropriado

--- aula02/front/src/routes/PrivateRoute.jsx
+++ aula02/front/src/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {Navigate}  from "react-router-dom"
 import { getMe } from "../services/usuarioService";
 
-export default function PrivateRoute({children}){
+export default function PrivateRoute({children, role}){
     const [loading, setLoading] = useState(true);
     const [usuario, setUsuario] = useState(null);
     
@@ -22,7 +22,8 @@ export default function PrivateRoute({children}){
     },[]);
 
     if(loading) return <p>Carregando...</p>
-    if(!usuario) return <Navigate to="/login" />
+    if(!usuario) return <Navigate to="/" />
+    if(role && usuario.role !== role) return <Navigate to="/filmes" replace />
 
     return children;
     
