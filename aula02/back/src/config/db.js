cdff5ec25d3249760cb643d857860cdd255a3b6b
Joel Santos <joeloliveira56@gmail.com>
2026-04-09 20:57:41 -0300

    feat: refatora a lógica de conexão com o banco de dados e inicializa tabelas

--- aula02/back/src/config/db.js
+++ aula02/back/src/config/db.js
@@ -1,21 +1,14 @@
 import pkg from "pg";
-const { Pool } = pkg;
+const {Pool} = pkg;
 
-let pool;
+const db = new Pool(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+    }
+)
 
-if (!global.pool) {
-  global.pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-}
-
-pool = global.pool;
-
-export default pool;
+export default db;
