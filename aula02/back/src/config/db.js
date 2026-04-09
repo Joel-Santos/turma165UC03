b67cef3a18ac60c875287b4cbf31367b4e216a64
Joel Santos <joeloliveira56@gmail.com>
2026-04-09 20:55:17 -0300

    feat: refatora a lógica de conexão com o banco de dados para uso de pool global

--- aula02/back/src/config/db.js
+++ aula02/back/src/config/db.js
@@ -1,14 +1,21 @@
 import pkg from "pg";
-const {Pool} = pkg;
+const { Pool } = pkg;
 
-const db = new Pool(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
-    }
-)
+let pool;
 
-export default db;
+if (!global.pool) {
+  global.pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
+
+pool = global.pool;
+
+export default pool;
